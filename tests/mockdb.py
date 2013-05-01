524c2607de778527d79103edd21e53797ed8667e
Evan Friis <ekfriis@gmail.com>
2013-05-01 13:27:19 +0200

    Interface to test postgres DB for unit tests

--- tests/mockdb.py
+++ tests/mockdb.py
@@ -6,9 +6,15 @@ Create a simple PostGIS test DB for unit tests
 
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from geoalchemy import GeometryDDL
 
-from models import OSRMEdge, OSRMNode  # flake8: noqa
+from stanalysis.models import OSRMEdge, OSRMNode, Base  # flake8: noqa
 
-engine = create_engine('postgresql://osm:osm@localhost/osm', echo=True)
+engine = create_engine('postgresql://osm:osm@localhost/osm_test', echo=True)
 Session = sessionmaker(bind=engine)
 session = Session()
+
+def create_tables():
+    """Drop existing test tables and create new ones"""
+    Base.metadata.drop_all(engine)
+    Base.metadata.create_all(engine)
