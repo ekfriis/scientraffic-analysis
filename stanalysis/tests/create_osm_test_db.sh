5c80179356e726b49d2d44fc4a02dc0cb6dd09d8
Evan Friis <ekfriis@gmail.com>
2013-05-01 15:39:40 +0200

    Fix ownership problem with test db

--- stanalysis/tests/create_osm_test_db.sh
+++ stanalysis/tests/create_osm_test_db.sh
@@ -5,6 +5,6 @@ psql -d osm_test -f /usr/share/postgresql/9.1/contrib/postgis-1.5/postgis.sql
 psql -d osm_test -f /usr/share/postgresql/9.1/contrib/postgis-1.5/spatial_ref_sys.sql
 
 #Grant permissions to user 'gis' on the new database
-echo "grant all on database osm_test to osm;" | psql -d osm_test
-echo "grant all on spatial_ref_sys to osm;" | psql -d osm_test
-echo "grant all on geometry_columns to osm;" | psql -d osm_test
+echo "ALTER TABLE geometry_columns OWNER TO osm;" | psql -d osm_test
+echo "ALTER TABLE spatial_ref_sys OWNER TO osm;" | psql -d osm_test
+echo "ALTER USER osm WITH PASSWORD 'osm';" | psql -d osm_test
