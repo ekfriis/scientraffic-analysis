2932755d775be5c94c84077b84a5b7f75c6b1d2c
Evan Friis <ekfriis@gmail.com>
2013-05-01 13:17:44 +0200

    Fix bugs in DB creator script

--- tests/create_osm_test_db.sh
+++ tests/create_osm_test_db.sh
@@ -1,12 +1,10 @@
+#!/bin/bash
 createdb -E UNICODE osm_test
 createlang plpgsql osm_test
 psql -d osm_test -f /usr/share/postgresql/9.1/contrib/postgis-1.5/postgis.sql
-psql -d osm_test -f
-/usr/share/postgresql/9.1/contrib/postgis-1.5/spatial_ref_sys.sql
+psql -d osm_test -f /usr/share/postgresql/9.1/contrib/postgis-1.5/spatial_ref_sys.sql
 
 #Grant permissions to user 'gis' on the new database
-psql osm_test
-grant all on database osm_test to "osm";
-grant all on spatial_ref_sys to "osm";
-grant all on geometry_columns to "osm";
-\q
+echo "grant all on database osm_test to osm;" | psql -d osm_test
+echo "grant all on spatial_ref_sys to osm;" | psql -d osm_test
+echo "grant all on geometry_columns to osm;" | psql -d osm_test
