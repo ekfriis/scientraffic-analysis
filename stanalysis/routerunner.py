c9fda22c2e1b48b2b53e47bd25502d9d9855074c
Evan Friis <ekfriis@gmail.com>
2013-05-04 13:01:40 +0200

    Factor out tools for doing toy routes

---
+++ stanalysis/routerunner.py
@@ -0,0 +1,79 @@
+# -*- coding: utf-8 -*-
+"""
+Tools to run random routes using the OSRM
+"""
+
+import logging
+import numpy as np
+import requests
+
+log = logging.getLogger(__name__)
+
+
+def generate_random_choices(N, alist):
+    """ Generate random choices from a list
+
+    :param: N - number of choices to generate
+    :param: alist - a random access iterable to select from
+
+    >>> np.random.seed(0xDEADBEEF)
+    >>>
+    """
+    for _ in range(N):
+        start_idx = np.random.randint(0, len(alist)-1)
+        end_idx = np.random.randint(0, len(alist)-1)
+        # protect against unlikely case
+        while end_idx == start_idx:
+            end_idx = np.random.randint(0, len(alist)-1)
+        yield (alist[start_idx], alist[end_idx])
+
+
+def build_osrm_url(coords, host, port):
+    """Build an OSRM query URL
+
+    :param: coords - a two-tuple of integer (lat, lon) pairs.
+        They should be scaled up by 1E5.
+    :param: host - hostname of OSRM server
+    :param: port - port of OSRM server
+    """
+    startpt, endpt = coords
+    # we can't uses the requests params feature, since it
+    # will escape the commas.
+    url = 'http://{host}:{port}/viaroute?loc={loc1}'\
+        '&loc={loc2}&instructions=false&raw=true'.format(
+            host=host, port=port,
+            loc1=','.join('%0.6f' % (x / 1E5) for x in startpt),
+            loc2=','.join('%0.6f' % (x / 1E5) for x in endpt),
+        )
+    return url
+
+
+def parse_osrm_output(response):
+    """Parse the JSON object returned by OSRM
+
+    Returns 4-D numpy array raw node steps, where the second
+    axis has the form
+
+    (node_id, duration, lat, lon)
+
+    """
+    return np.array(response.json()['raw_data'], dtype=int)
+
+
+def run_route(coords, host='localhost', port=5000):
+    """Query an OSRM server for a route
+
+    Returns the (decoded) JSON `output from OSRM`_.
+
+    .. _output from OSRM: https://github.com/DennisOSRM/
+        Project-OSRM/wiki/Output-json
+
+    :param coords: 2-tuple of starting & ending (lat, lon)
+    """
+    url = build_osrm_url(coords, host, port)
+    response = requests.get(url)
+    if response.status_code != 200:
+        log.error("Route lookup with %s failed with %i.",
+                  url, response.status_code)
+        return None
+    return parse_osrm_output(response)
---
+++ stanalysis/tests/test_routerunner.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+
+import numpy
+from nose.tools import eq_
+
+from stanalysis.routerunner import generate_random_choices, build_osrm_url,\
+    parse_osrm_output
+
+
+def test_generate_random_choice():
+    numpy.random.seed(0xDEADBEEF)
+    eq_(list(generate_random_choices(5, range(10))),
+        [(8, 1), (2, 1), (2, 5), (1, 7), (4, 0)])
+
+
+def test_build_osrm_url():
+    eq_(build_osrm_url(((34E5, -118E5), (35E5, -118E5)), 'the_host', 9999),
+        'http://the_host:9999/viaroute?loc=34.000000,-118.000000&'
+        'loc=35.000000,-118.000000&instructions=false&raw=true')
+
+
+def test_parse_osrm_output():
+
+    class MockResponse():
+        def json(self):
+            return {
+                'raw_data': [
+                    [0, 1, 2, 3],
+                    [4, 5, 6, 7]
+                ]
+            }
+
+    result = parse_osrm_output(MockResponse())
+    assert(numpy.array_equal(
+        result, numpy.array([[0, 1, 2, 3], [4, 5, 6, 7]], dtype=int)))
