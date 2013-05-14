d334a6dcf12f9b54b4f34189557e1a679ecf3d5c
Evan Friis <ekfriis@gmail.com>
2013-05-14 21:41:03 +0200

    Add SQL script to build routed nodes with geometries

--- build-views.sql
+++ build-views.sql
@@ -1,10 +1,21 @@
 -- run me as: psql osrm osm < build-views.sql
 
-CREATE OR REPLACE VIEW routed_edges AS
+--CREATE OR REPLACE VIEW routed_edges AS
+  --SELECT 
+    --step.edge_id AS edge, 
+    --COUNT(step.edge_id) AS freq,
+    --geoms.geom AS geom
+  --FROM osrmroutestep AS step
+  --INNER JOIN osrmedgegeoms AS geoms ON geoms.hash = step.edge_id
+  --GROUP BY step.edge_id, geoms.geom;
+
+CREATE OR REPLACE VIEW routenodesgeom AS
   SELECT 
-    step.edge_id AS edge, 
-    COUNT(step.edge_id) AS freq,
-    geoms.geom AS geom
-  FROM osrmroutestep AS step
-  INNER JOIN osrmedgegeoms AS geoms ON geoms.hash = step.edge_id
-  GROUP BY step.edge_id, geoms.geom;
+    routenode.osm_id AS id, 
+    routenode.n_outputs AS n_outputs,
+    routenode.sum_out AS sum_out,
+    routenode.product_out AS product_out,
+    routenode.log_product_out AS log_product_out,
+    node.geom AS geom
+  FROM routenodes AS routenode
+  INNER JOIN osrmnodes AS node ON node.osm_id = routenode.osm_id;
