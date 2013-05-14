2fd35fa6da98d505be814922bae0a7cd0f6f3f06
Evan Friis <ekfriis@gmail.com>
2013-05-14 04:08:48 +0200

    Hooray! Much faster.

--- edge-frequencies.sql
+++ edge-frequencies.sql
@@ -3,16 +3,16 @@
 
 -- run me as: psql osrm osm < edge-frequencies.sql
 
-DROP TABLE edgefrequencies;
+DROP TABLE edgefrequencies_tmp;
 
-CREATE TABLE edgefrequencies (
+CREATE TABLE edgefrequencies_tmp (
   edge BIGINT NOT NULL,
   forward BOOLEAN NOT NULL,
   freq BIGINT,
   PRIMARY KEY (edge, forward)
 );
 
-INSERT INTO  edgefrequencies (edge, forward, freq)
+INSERT INTO  edgefrequencies_tmp (edge, forward, freq)
 SELECT 
   step.edge_id AS edge, 
   step.forward AS forward, 
@@ -21,16 +21,27 @@ FROM
   osrmroutesteps AS step
   GROUP BY step.edge_id, step.forward;
 
-CREATE INDEX "idx_edgefrequences_edge" ON edgefrequencies (edge);
+CREATE INDEX "idx_edgefrequences_edge" ON edgefrequencies_tmp (edge);
 
--- Add geometry column for easy QGIS viewing.
+-- Now do a version with populated geometry for easy QGIS viewing.
+DROP TABLE edgefrequencies;
+CREATE TABLE edgefrequencies (
+  edge BIGINT NOT NULL,
+  forward BOOLEAN NOT NULL,
+  freq BIGINT,
+  PRIMARY KEY (edge, forward)
+);
+
 SELECT AddGeometryColumn('public', 'edgefrequencies', 'geom', 4326, 'LINESTRING', 2);
 
--- Populate geometry
-EXPLAIN UPDATE edgefrequencies freq SET geom = geoms.geom
-FROM osrmedgegeoms geoms WHERE freq.edge = geoms.hash;
-
-UPDATE edgefrequencies freq SET geom = geoms.geom
-FROM osrmedgegeoms geoms WHERE freq.edge = geoms.hash;
+INSERT INTO edgefrequencies (edge, forward, freq, geom)
+SELECT 
+  edges.edge,
+  edges.forward,
+  edges.freq,
+  geoms.geom
+FROM edgefrequencies_tmp AS edges
+INNER JOIN osrmedgegeoms AS geoms ON geoms.hash = edges.edge;
 
 CREATE INDEX "idx_edgefrequencies_geom" ON "public"."edgefrequencies" USING GIST (geom);
+DROP TABLE edgefrequencies_tmp;
