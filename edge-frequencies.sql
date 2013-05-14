4b339a3bc5f974a305e0baf40b8680e2edcda1c6
Evan Friis <ekfriis@gmail.com>
2013-05-14 04:02:25 +0200

    NWO for edge frequencies, but it is slow.

--- edge-frequencies.sql
+++ edge-frequencies.sql
@@ -5,25 +5,32 @@
 
 DROP TABLE edgefrequencies;
 
-EXPLAIN SELECT 
-  step.edge_id AS edge, 
-  step.forward AS forward, 
-  COUNT(*) AS freq,
-  geoms.geom AS geom
-INTO 
-  edgefrequencies
-FROM 
-  osrmroutesteps AS step
-INNER JOIN 
-  osrmedgegeoms AS geoms ON geoms.hash = step.edge_id
-GROUP BY step.edge_id, step.forward, geoms.geom;
+CREATE TABLE edgefrequencies (
+  edge BIGINT NOT NULL,
+  forward BOOLEAN NOT NULL,
+  freq BIGINT,
+  PRIMARY KEY (edge, forward)
+);
 
-EXPLAIN SELECT 
+INSERT INTO  edgefrequencies (edge, forward, freq)
+SELECT 
   step.edge_id AS edge, 
   step.forward AS forward, 
   COUNT(*) AS freq
-INTO 
-  edgefrequencies
 FROM 
   osrmroutesteps AS step
-GROUP BY step.edge_id, step.forward;
+  GROUP BY step.edge_id, step.forward;
+
+CREATE INDEX "idx_edgefrequences_edge" ON edgefrequencies (edge);
+
+-- Add geometry column for easy QGIS viewing.
+SELECT AddGeometryColumn('public', 'edgefrequencies', 'geom', 4326, 'LINESTRING', 2);
+
+-- Populate geometry
+EXPLAIN UPDATE edgefrequencies freq SET geom = geoms.geom
+FROM osrmedgegeoms geoms WHERE freq.edge = geoms.hash;
+
+UPDATE edgefrequencies freq SET geom = geoms.geom
+FROM osrmedgegeoms geoms WHERE freq.edge = geoms.hash;
+
+CREATE INDEX "idx_edgefrequencies_geom" ON "public"."edgefrequencies" USING GIST (geom);
