805a5f38338b0df3a298edb8f8fa08cbe0eff753
Evan Friis <ekfriis@gmail.com>
2013-05-15 07:36:05 +0200

    Add redundancy flag

--- build-views.sql
+++ build-views.sql
@@ -15,7 +15,8 @@ CREATE OR REPLACE VIEW routenodesgeom AS
     routenode.n_outputs AS n_outputs,
     routenode.sum_out AS sum_out,
     routenode.product_out AS product_out,
-    routenode.log_product_out AS log_product_out,
+    routenode.log_sum_out AS log_sum_out,
+    routenode.redundant AS redundant,
     node.geom AS geom
   FROM routenodes AS routenode
   INNER JOIN osrmnodes AS node ON node.osm_id = routenode.osm_id;
