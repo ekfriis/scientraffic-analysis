d7e13e386614a704c500f039b95f424d6b5c3619
Evan Friis <ekfriis@gmail.com>
2013-06-18 05:32:08 +0200

    cleanup

--- polygonize-edge-frequencies.sql
+++ polygonize-edge-frequencies.sql
@@ -10,10 +10,3 @@ FROM (
   SELECT ST_Polygonize(ST_LineMerge(ST_Multi(geom))) AS polycoll
   FROM edgefrequencies_bothways WHERE freq > 1
 ) AS foo;
-
--- select ST_AsText(ST_Polygonize(ST_LineMerge(ST_Multi(geom)))) from edgefrequencies_bothways where freq > 1;
-
---SELECT ST_AsEWKT((ST_Dump(foofoo.polycoll)).geom) As geomtextrep
---FROM (SELECT ST_Polygonize(the_geom_4269) As polycoll
-        --FROM (SELECT the_geom_4269 FROM ma.suffolk_edges
-                        --ORDER BY tlid LIMIT 45) As foo) As foofoo;
