fdda99c8ac06886d2a915fcfdecac307971e1e47
Evan Friis <ekfriis@gmail.com>
2013-06-18 04:34:41 +0200

    Add tool for importing census tracts

---
+++ import-census-tracts.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# Downloads (if necessary) and imports census tract data into PostGIS
+
+if [ ! -f tl_2011_06_tract.zip ];
+then
+  wget http://www2.census.gov/geo/tiger/TIGER2011/TRACT/tl_2011_06_tract.zip
+fi
+
+unzip -o tl_2011_06_tract.zip
+
+# Note: I don't undertand the -nlt option, but see:
+# http://trac.osgeo.org/gdal/ticket/4939
+
+ogr2ogr -overwrite -nlt MULTIPOLYGON -f "PostgreSQL" \
+  PG:"host=localhost user=osm dbname=osrm password=osm" \
+  tl_2011_06_tract.shp \
+  -clipdst -119.44628 33.13301 -116.96712 34.42179 \
+  -clipdstsql "WHERE ALAND > 0"  # ignore water only tracts
+
+
+ls tl_2011_06_tract.* | grep -v zip | xargs rm
+
+# Buffer 
+echo "Creating dissolved census tracts"
+echo "DROP TABLE tl_2011_06_tract_buffered;" | psql osrm osm
+echo "select aland, ogc_fid, ST_Buffer(wkb_geometry, -1E-3) INTO tl_2011_06_tract_buffered from tl_2011_06_tract where aland > 0;" | psql osrm osm
